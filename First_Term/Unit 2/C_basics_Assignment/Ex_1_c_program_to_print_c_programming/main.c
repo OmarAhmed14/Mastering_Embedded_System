e8de61191846b6316f679abb853f60ac7e45a12d
Omar Ahmed <Oa0578222@gmail.com>
2022-07-16 11:48:59 +0200

    First_Term

---
+++ First_Term/Unit 2/C_basics_Assignment/Ex_1_c_program_to_print_c_programming/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 15, 2022
+ *      Author: OmarAhmed
+ */
+
+//EX1:
+//Write C Program to Print a Sentence
+//i should see the Console as following:
+//##########Console-output###
+//C Programming
+//###########################
+
+#include "stdio.h"
+
+int main ()
+{
+   printf ("C Programming \n");
+return 0;
+
+}
