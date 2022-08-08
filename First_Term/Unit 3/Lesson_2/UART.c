d42dfcd82b48bae21027e26a8402513a360c072b
Omar Ahmed <Oa0578222@gmail.com>
2022-08-09 00:44:07 +0200

    Embedded C Code From Scratch

---
+++ First_Term/Unit 3/Lesson_2/UART.c
@@ -0,0 +1,14 @@
+#include "UART.h"
+
+
+#define UART0DR *((volatile unsigned int *) ((unsigned int *) 0x101f1000))
+
+
+void UART_string (unsigned char* p)
+{
+	while (*p != 0)
+	{
+		UART0DR = (unsigned int*) *p;
+		p++;
+	}
+}
