1108fa37ba6b3663d4b84735dbaf6cae5f7ba082
Omar Ahmed <Oa0578222@gmail.com>
2022-08-12 16:09:46 +0200

    First Term

--- First_Term/Unit 3/Lesson 3/main.c
+++ First_Term/Unit 3/Lesson 3/main.c
@@ -10,16 +10,17 @@
 
 #define RCC_BASE     0x40021000
 #define GPIO_PA_BASE 0x40010800
-#define RCC_APB2ENA  *(volatile int32_t*)(RCC_BASE+0x18)
-#define GPIO_PA_CRH  *(volatile int32_t*)(GPIO_PA_BASE+0x04)
-#define GPIO_PA_ODR  *(volatile int32_t*)(GPIO_PA_BASE+0x0C)
-int arr [] = {1,2,3};
-int arr2 [10];
+#define RCC_APB2ENA  *(volatile uint32_t*)(RCC_BASE+0x18)
+#define GPIO_PA_CRH  *(volatile uint32_t*)(GPIO_PA_BASE+0x04)
+#define GPIO_PA_ODR  *(volatile uint32_t*)(GPIO_PA_BASE+0x0C)
+
+int arr[] = {1,2,3};
+int arr2[10];
 
 int main ()
 {
 	int i;
-	RCC_APB2ENA |= 0x04;
+	RCC_APB2ENA |= 0x04;   // (1<<2)     
 	GPIO_PA_CRH &= 0xff0fffff;
 	GPIO_PA_CRH |= 0x00200000;
 
